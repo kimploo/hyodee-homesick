706c780fb9d4cb0094e105952ffe12b8a9223b4a
Hong Shik Branden Kim <kimploo@gmail.com>
2021-05-23 01:27:23 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  display: block;
+`;
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">test</header>
+    </div>
+  );
+};
